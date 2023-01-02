b02d0f16403ba2c3167df6d701f852e54b0fa2f2
EC2 Default User <ec2-user@ip-172-31-28-191.ec2.internal>
2023-01-02 12:59:37 +0000

    [Update] bookのshowページまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,32 @@
 class BooksController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @new_book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
+      redirect_to book_path(@new_book.id)
+    else
+      render :index
+    end
   end
 
   def show
+    @new_book = Book.new
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user.id)
   end
 
   def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @new_book = Book.new
+    @books = @user.books
   end
 
   def edit
